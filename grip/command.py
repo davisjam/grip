081e26d4712003c70a58dcef14f21c05cfbd6cbb
Jamie Davis <davisjam@vt.edu>
2016-11-16 14:09:09 -0500

    grip: cleanup

--- grip/command.py
+++ grip/command.py
@@ -45,6 +45,7 @@ from __future__ import print_function
 import sys
 import mimetypes
 import socket
+import errno
 
 from docopt import docopt
 from getpass import getpass
@@ -132,5 +133,6 @@ def main(argv=None, force_utf8=True, patch_svg=True):
         return 1
     except socket.error as ex:
         print('Error:', ex)
-        print('Is a grip server already running? Kill it or specify another port.')
+        if ex.errno == errno.EADDRINUSE:
+            print('This port is in use. Is a grip server already running? Kill it or specify another port.')
         return 1
